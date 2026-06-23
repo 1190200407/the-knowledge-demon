177f1f60aa57d374ab41d46c94396d4ea1bc31ce
ComicChess <928007032@qq.com>
2026-06-23 22:15:26 +0800

    新卡实体化

---
+++ Scripts/Cards/Embodiment.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Keywords;
+
+namespace ComicChess.KnowledgeDemon;
+
+[RegisterCard(typeof(KnowledgeDemonCardPool))]
+public sealed class Embodiment : KnowledgeDemonCardModel
+{
+    private const int energyCost = 2;
+    private const CardType type = CardType.Attack;
+    private const CardRarity rarity = CardRarity.Rare;
+    private const TargetType targetType = TargetType.AnyEnemy;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+        [ModKeywordRegistry.GetCardKeyword(KnowledgeDemonKeyword.Choose)];
+
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+        [KnowledgeDemonKeywordHoverTips.FromChoose()];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(12m, ValueProp.Move)];
+
+    public Embodiment()
+        : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_blunt")
+            .Execute(choiceContext);
+
+        var libraryPile = BookLibraryUtility.TryGetLibraryPile(Owner);
+        if (libraryPile is not null)
+        {
+            var zeroCostCards = libraryPile.Cards
+                .Where(card => !card.EnergyCost.CostsX && card.EnergyCost.GetWithModifiers(CostModifiers.All) == 0)
+                .ToList();
+
+            foreach (var card in zeroCostCards)
+            {
+                await CardPileCmd.Add(card, PileType.Hand);
+            }
+        }
+
+        await BookLibraryCmd.ChooseFromLibraryAndAutoPlay(choiceContext, Owner, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4m);
+    }
+}
